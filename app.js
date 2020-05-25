d5c629647803e5d9af4f0a562b564dce474540bd
Matt Andrews <matt@carbonbasedcreative.com>
2020-05-24 19:11:28 -0600

    Simple Area and Perimeter Module

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const rect = {
-    perimeter: (x, y) => 2 * (x+y),
-    area: (x,y) => x * yield
-};
+const rect = require('./rectangle');
 
 function solvedRect(l, w) {
     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
+
+    if(l <= 0 || w <= 0 ) {
+        console.log(`Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`);
+    } else {
+        console.log(`Area of rectangle: ${rect.area(l,w)} `);
+        console.log(`Perimiter of rectangle: ${rect.perimeter(l,w)} `);
+    }
 }
+
+solvedRect(2,4);
+solvedRect(3,5);
+solvedRect(0,5);
+solvedRect(0,10);
