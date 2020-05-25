bb7aacc288a2c50840510a9f76710a65f43a7e03
Matt Andrews <matt@carbonbasedcreative.com>
2020-05-24 19:01:21 -0600

    Intial simple node module

---
+++ app.js
@@ -0,0 +1,8 @@
+const rect = {
+    perimeter: (x, y) => 2 * (x+y),
+    area: (x,y) => x * yield
+};
+
+function solvedRect(l, w) {
+    console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
+}
